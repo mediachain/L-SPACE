baa0d4d86a8680d0050418f683e6043a1115724f
Yusef Napora <yusef@napora.org>
2016-03-30 12:06:01 -0400

    add node project to visualize graph

---
+++ visualizer/src/app/init-cytoscape.js
@@ -0,0 +1,48 @@
+import cytoscape from 'cytoscape';
+
+const StaticOpts = {
+  layout: { name: 'cola' },
+
+  // initial viewport state:
+  zoom: 1,
+  pan: { x: 0, y: 0 },
+
+  // interaction options:
+  minZoom: 1e-50,
+  maxZoom: 1e50,
+  zoomingEnabled: true,
+  userZoomingEnabled: true,
+  panningEnabled: true,
+  userPanningEnabled: true,
+  boxSelectionEnabled: false,
+  selectionType: 'single',
+  touchTapThreshold: 8,
+  desktopTapThreshold: 4,
+  autolock: false,
+  autoungrabify: false,
+  autounselectify: false,
+
+  // rendering options:
+  headless: false,
+  styleEnabled: true,
+  hideEdgesOnViewport: false,
+  hideLabelsOnViewport: false,
+  textureOnViewport: false,
+  motionBlur: false,
+  motionBlurOpacity: 0.2,
+  wheelSensitivity: 1,
+  pixelRatio: 'auto'
+};
+
+function initCytoscape(containerId, elements, style) = {
+  const opts = {
+    ...StaticOpts,
+    container: containerId,
+    elements: elements,
+    style: style
+  };
+
+  return cytoscape(opts);
+};
+
+export default initCytoscape;
